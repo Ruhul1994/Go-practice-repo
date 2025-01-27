4d95306d76e5040e83a9bb4527928dd1f5104439
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-01-27 22:55:19 +0600

    some new variables added

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 import "fmt"
-func main () {
-	 a := 10
-	 b := 20
-	c := a + b
-	fmt.Println("The sum of a and b is: ", c);
-	fmt.Println("Thank you Ruhul you did it")
+// func main () {
+// 	 a := 10
+// 	 b := 20
+// 	c := a + b
+// 	fmt.Println("The sum of a and b is: ", c);
+// 	fmt.Println("Thank you Ruhul you did it")
+// }
+
+func main() {
+	fmt.Println("starting the program...")
+	great("Ruhul")
+}
+
+func great(name string) {
+	fmt.Printf("Hello, %s\n", name)
 }
