da0cc1493fdd8f9edc3898242fc2314f6a7ecb02
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-01-27 22:58:18 +0600

    some variable are added

--- main.go
+++ main.go
@@ -8,11 +8,21 @@ import "fmt"
 // 	fmt.Println("Thank you Ruhul you did it")
 // }
 
-func main() {
-	fmt.Println("starting the program...")
-	great("Ruhul")
-}
+// func main() {
+// 	fmt.Println("starting the program...")
+// 	great("Ruhul")
+// }
 
-func great(name string) {
-	fmt.Printf("Hello, %s\n", name)
+// func great(name string) {
+// 	fmt.Printf("Hello, %s\n", name)
+// }
+func main() {
+	 name, location := "Ruhul", "Dhaka"
+	 var age int = 25
+	 var pi float64 = 3.1416
+
+	 fmt.Println(name, location, age, pi);
+
+	 const language = "Go"
+	 fmt.Printf("The language is %s\n", language)
 }
